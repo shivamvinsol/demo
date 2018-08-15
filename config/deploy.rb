f03235900d93ebf35c067ffb8ba9530d30c59a67
Shivam Jain <shivam@vinsol.com>
2018-08-16 01:09:25 +0530

    update pg version,deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :keep_releases, 5
 set :stage,           :production
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 
 namespace :deploy do
   after :publishing, 'deploy:restart'
