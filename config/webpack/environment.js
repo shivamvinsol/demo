f53b92e016751d9dacb3b7e882cf794fee96c56b
Shivam Jain <shivam@vinsol.com>
2018-08-30 03:02:05 +0530

    minor package changes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,14 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}))
-
 module.exports = environment
+
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
